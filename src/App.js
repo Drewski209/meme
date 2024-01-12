4ed071a62d045bb6e2e430eae0403c6b7cc53cf1
drewsolis117@gmail.com <drewsolis117@gmail.com>
2024-01-11 16:14:24 -0800

    commit two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from './Header';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div>
+    <Header/>
     </div>
   );
 }
-
-export default App;
